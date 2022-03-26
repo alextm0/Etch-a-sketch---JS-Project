98275bd50cb5d48c9daf9c4026b1d3f61285b7dc
alextm <alextoma1704@gmail.com>
2022-03-26 13:29:49 +0200

    sa dea un ochi sefu

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-var container = document.querySelector(".container");
+const DIMENSION = 8;
 
+var container = document.querySelector(".container");
 var board = document.createElement('board');
 
-for(i = 0; i < 16; i++) {
-  // Create row
-  var row = document.createElement('tr');
-  for(j = 0; j < 16; j++) {
-    // Create a cell
-    var cell = document.createElement('td');
-
-    if((i + j) % 2 == 0) {
-      cell.setAttribute('class', 'cell whitecell');
-      row.append(cell);
+function generateBoard() {
+  for(i = 0; i < DIMENSION; i++) {
+    // Create row
+    var row = document.createElement('tr');
+    for(j = 0; j < DIMENSION; j++) {
+      // Create a cell
+      var cell = document.createElement('td');
+  
+      if((i + j) % 2 == 0) {
+        cell.setAttribute('class', 'cell whitecell');
+        row.append(cell);
+      }
+      else {
+        cell.setAttribute('class', 'cell blackcell');
+        row.append(cell);
+      }
     }
-    else {
-      cell.setAttribute('class', 'cell blackcell');
-      row.append(cell);
-    }
+    board.append(row);
   }
-  board.append(row);
+  
+  container.append(board);
 }
 
-container.append(board);
+generateBoard();
+
+var colorButton = document.querySelector
